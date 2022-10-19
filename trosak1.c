9b2edfbfb7f385eb63ebfc6cd6d99795bde9bb09
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2022-10-19 16:01:59 +0200

    Create trosak1.c

---
+++ trosak1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+float minTrosak(int N, float x){   //definicija funkcije
+	return N*x;
+}
+float minTrosak2(int N, float x=15.99){  //x ima zadani argument
+	return N*x;
+}
+	int main(){
+		int brPrijatelja;
+		float cijena, trosak;
+		
+		printf("Unesi broj prijatelja: ");
+		scanf("%d", &brPrijatelja);
+		printf("Unesi cijenu pretplate: ");
+		scanf("%f", &cijena);
+		
+		trosak=minTrosak(brPrijatelja, cijena);    //poziv funkcije
+		printf("Ukupni trosak iznosi %.2f novaca", trosak);
+		trosak=minTrosak2(brPrijatelja);    //poziv funkcije
+		printf("\nUkupni trosak iznosi %.2f novaca", trosak);
+		
+		
+		
+		return 0;
+	}
