d17d5c624c05745f7edf2128314bef90ccf413e3
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2022-12-16 09:33:57 +0100

    Add files via upload

---
+++ pravokutnik i boje.cpp
@@ -0,0 +1,42 @@
+//za svaki plavi pravokutnik treba izraèunati površinu, za svaki naranèasti treba izraèunati opseg, svaki crveni trebaizbaciti veæu stranicu
+//svaki crveni trebaizbaciti veæu stranicu, ostali izbaciti manju str
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct praokutnik{
+	int R;
+	int G;
+	int B;
+	int a;
+	int c;
+};
+
+int main(){
+	
+	int prvi;
+	int drugi;
+	int treci;
+	int cetvrti;
+	
+
+
+
+struct pravokutnik
+
+	p[4]={40,0,0,5,7,
+				0,0,79,7,2,
+				120,60,5,4,
+				40,29,90,4,4};
+				
+	for(i=0;i<4;i++)
+		if(p[i].R==0&&p[i].G==0&&p[i].B>0) printf("ovo je plavi: ")
+		else if(p[i].R>0&&p[i].G==0&&p[i].B==0) printf("ovo je crveni")
+
+	
+	
+}
+
+
