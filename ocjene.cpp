3596c9f6950c2a89a53ee82b80bf6267b2b2079f
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2023-03-22 12:25:18 +0100

    Add files via upload

---
+++ ocjene.cpp
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+#include<stdlib.h>
+
+struct skola {
+	int ocjena;
+   char ime[20];
+   char predmet[20];
+	
+}s;
+
+void unos(){
+
+	FILE*filePointer;
+	filePointer=fopen("ocjene.txt","a");
+
+	printf("ime ucenika?");
+	scanf("%s", s.ime);
+	printf("ime predmeta?");
+	scanf("%s", s.predmet);
+	printf("Kolika mu je ocjena?");
+	scanf("%d", &s.ocjena);
+
+	fprintf(filePointer, "%s %s %d",s.ime, s.predmet, s.ocjena);
+	fclose(filePointer);
+	printf("Pritisnite bilo koju tipku za nastavak");
+	getch();
+	
+
+} 
+
+void prosjek_oc(){
+	
+	FILE*filePointer;
+	filePointer=fopen("ocjene.txt","a");
+	
+	int i=0;
+	int sum=0;
+	{
+	while(fscanf(filePointer, "%s %s %d",s.ime, s.predmet, s.ocjena)){
+		sum=s.ocjena;
+		i++;
+	}
+	printf("prosjek ocjena je %.2f", (float) sum/i);
+
+
+	fclose(filePointer);
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+	
+int odg;
+
+do{
+	system("cls");
+	printf("\t1.Unos ocjene\t\n");
+	printf("\t2.prosjek ocjena\t\n");
+	printf("\t3.Broj predmeta\t\n");
+	printf("\t4.Prosjek\t\n");
+	printf("\t5.Ispis\t\n");
+	printf("\t6.Exit\t\n");
+	odg=getch();
+
+	switch(odg){
+			case '1': 
+				unos();
+				break;
+			case '2': 
+				prosjek_oc();
+				break;
+			case '3': 
+				//br_pred();
+				break;
+			case '4': 
+				//prosjek_pred();
+				break;
+			case '5': 
+				//ispis()
+				break;
+			case '6':
+				//exit();
+				break;
+			default:
+				printf("de pise ti od 1 do 6\n");
+		}
+
+
+
+} while (odg!='6');
+}
